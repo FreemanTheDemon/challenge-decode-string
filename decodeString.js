525070ece5d5c2b7e8fe57f3ccd049afec65250f
Freeman Bird <freemanbird@gmail.com>
2021-11-29 09:35:54 -0700

    completed challenge

--- decodeString.js
+++ decodeString.js
@@ -1,1 +1,28 @@
 // Write your code below this line
+// >>> "1a" // "b"
+// >>> "3ce" // "fh"
+// >>> "2fcjjm" // "hello"
+
+const decoder = (str) => {
+    // define the entire alphabet
+    let alpha = 'abcdefghijklmnopqrstuvwxyz';
+    // create a result string to return later
+    let result = '';
+    // get the first character and convert it into a number (in base 10)
+    let shift = parseInt(str[0], 10);
+    // loop over the input string, starting one after the start of the string
+    for (let i = 1; i < str.length; i++) {
+        // define a helper variable
+        let currentChar = str[i];
+        // define the new position by using indexOf and shifting it by the shift we defined earlier
+        let charPos = alpha.indexOf(currentChar) + shift;
+        // add the shifted character to the result string
+        result += alpha[charPos];
+    }
+    // return the result after all is well
+    return result;
+}
+
+console.log(decoder('1a'));
+console.log(decoder('3ce'));
+console.log(decoder('2fcjjm'));
